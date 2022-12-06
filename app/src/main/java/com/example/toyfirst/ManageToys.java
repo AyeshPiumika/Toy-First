d36eb229549b15b8487676b23fdc5f11ce7368a2
AyeshPiumika <piumikaayesh@gmail.com>
2022-12-06 22:37:46 +0530

    Update Interfaces

---
+++ app/src/main/java/com/example/toyfirst/ManageToys.java
@@ -0,0 +1,14 @@
+package com.example.toyfirst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageToys extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_toys);
+    }
+}
